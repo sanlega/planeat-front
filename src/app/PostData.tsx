093d722e896509ef62bf0bdf60f792f0d2f54f45
HakimHC <ahmedh90111@gmail.com>
2024-03-16 14:11:31 +0100

    fix

--- src/app/PostData.tsx
+++ src/app/PostData.tsx
@@ -1,30 +1,44 @@
 const handleSubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let data = await e.target.prompt.value;
-    const res = await fetch("http://10.19.200.106:8080/api/search", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-    console.log(res.json());
-    return (res.json())
+  const formData = new FormData(e.target);
+  const promptValue = formData.get("prompt");
+
+  try {
+    const res = await fetch("http://localhost:8080/api/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ prompt: promptValue })
+    });
+
+    if (res.ok) {
+      const js = await res.json();
+      console.log(js);
+      return js;
+    } else {
+      console.error("Failed to fetch data");
+      return {};
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    return {};
+  }
 };
 
 const PostData = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      id="prompt"
       className="w-full flex flex-row justify-end"
     >
-      <textarea name="prompt"
+      <textarea
+        name="prompt"
         placeholder="Make me a vegetarian meal with products of carrefour."
         className="max-h-96 min-h-12 h-10 w-2/5 rounded-xl flex flex-row justify-start bg-slate-100 p-3 text-gray-500"
       ></textarea>
-      <button>Send</button>
+      <button type="submit">Send</button>
     </form>
   );
 };
